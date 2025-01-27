c4de84a3f1489e5e73f8c673a49f153fadb75d2f
rockybhai-09 <coolcriminal786@gmail.com>
2025-01-27 13:09:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,15 +6,15 @@ class Config:
 
     SESSION_NAME = ""
 
-    API_ID = ""
+    API_ID = "27317669"
 
-    API_HASH = ""
+    API_HASH = "11b88c331c5d44fde57cf91de1a2156b"
 
-    CLIENT_ID = ""
+    CLIENT_ID = " 1059304887946-m2o1r3r4h13savpq9gp206al3jkltoac.apps.googleusercontent.com "
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [7326600267 6947378236]
 
     VIDEO_DESCRIPTION = ""
 
